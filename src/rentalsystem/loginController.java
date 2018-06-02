3feb7e70ed387aab63cb23bcc0f3ba97ee260e03
byarustev <byarus45@gmail.com>
2018-06-02 09:38:47 +0300

    first commit

---
+++ src/rentalsystem/loginController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentalsystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author steve
+ */
+public class loginController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void handleClose(ActionEvent event) {
+        System.exit(0);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
