74c1071c9722162234e9470ded2ca55e7007c545
Kasumba Robert <robein@ymail.com>
2018-06-11 10:19:08 +0300

    MORE WORK ON THE ADD TENANTS

--- src/controllers/RegisterTenantController.java
+++ src/controllers/RegisterTenantController.java
@@ -9,6 +9,7 @@ import GeneralClasses.Block;
 import GeneralClasses.House;
 import database.DatabaseHandler;
 import java.net.URL;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -34,16 +35,36 @@ public class RegisterTenantController implements Initializable {
       rentalsList = FXCollections.observableArrayList();
       rentalsCombo.setItems(rentalsList);
       rentalsCombo.setDisable(true);
+      //get a list of country codes from the Locale Library
+      String [] countryCodes = Locale.getISOCountries();
+      countriesList = FXCollections.observableArrayList();
+      for (String countryCode : countryCodes){
+            //pick the corresponding country names
+            Locale locale = new Locale("", countryCode);
+            String name = locale.getDisplayCountry();
+            countriesList.add(name);
+        }
+     
+      countriesCombo.setItems(countriesList);
+      
+      String [] idTypes = {"National ID", "Driving Permit","Passport"};
+      idTypeList =FXCollections.observableArrayList(idTypes);
+      idTypeCombo.setItems(idTypeList);
+      idTypeCombo.setEditable(true);
     }    
     
-    ObservableList blocksList,rentalsList; 
+    ObservableList blocksList,rentalsList,countriesList,idTypeList; 
      @FXML // fx:id="blockCombo"
     private ComboBox<Block> blockCombo; // Value injected by FXMLLoader
-     
+    
+    @FXML // fx:id="idTypeCombo"
+    private ComboBox<?> idTypeCombo; // Value injected by FXMLLoader
     @FXML // fx:id="rentalsCombo"
     private ComboBox<House> rentalsCombo; // Value injected by FXMLLoader
 
-
+    @FXML // fx:id="countriesCombo"
+    private ComboBox<String> countriesCombo; // Value injected by FXMLLoader
+        
     @FXML
     void changeRentals(MouseEvent event) {
        try{
@@ -61,10 +82,13 @@ public class RegisterTenantController implements Initializable {
        }
     }
 
+   
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert rentalsCombo != null : "fx:id=\"rentalsCombo\" was not injected: check your FXML file 'registerTenant.fxml'.";
+        assert countriesCombo != null : "fx:id=\"countriesCombo\" was not injected: check your FXML file 'registerTenant.fxml'.";
+        assert idTypeCombo != null : "fx:id=\"idTypeCombo\" was not injected: check your FXML file 'registerTenant.fxml'.";
         assert blockCombo != null : "fx:id=\"blockCombo\" was not injected: check your FXML file 'registerTenant.fxml'.";
+        assert rentalsCombo != null : "fx:id=\"rentalsCombo\" was not injected: check your FXML file 'registerTenant.fxml'.";
 
     }
 }
